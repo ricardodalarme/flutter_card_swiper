b763e8895748ebd010596df74a9d5a251f49e7b4
Ricardo Dalarme <ricardo.dalarme@profusion.mobi>
2023-01-12 22:34:04 -0300

    refactor(example): simplify the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,27 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:swipeable_cards_stack/swipeable_cards_stack.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(const MaterialApp(
+    title: 'Flutter Demo',
+    home: const MyHomePage(),
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -36,9 +23,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -68,8 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 debugPrint('onDown ${(widget as CardView).text} $index');
               }
             },
-            enableSwipeUp: true,
-            enableSwipeDown: true,
           ),
           Container(
             margin: const EdgeInsets.symmetric(vertical: 20.0),
