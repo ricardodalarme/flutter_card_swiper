7fb1f81c2c5b0de8cfedf159823be7e3c7f8ce5a
richertc <christian.richert@outlook.com>
2023-03-16 15:30:55 +0100

    remove early return

--- lib/src/card_swiper.dart
+++ lib/src/card_swiper.dart
@@ -274,17 +274,15 @@ class _CardSwiperState<T extends Widget> extends State<CardSwiper<T>>
         if (_swipeType == SwipeType.swipe) {
           final cancelSwipe = widget.onSwipe?.call(_currentIndex, detectedDirection) == false;
 
-          if (cancelSwipe) {
-            return;
-          }
+          if (!cancelSwipe) {
+            _stack.removeAt(_currentIndex);
 
-          _stack.removeAt(_currentIndex);
+            if (_stack.isEmpty) {
+              widget.onEnd?.call();
 
-          if (_stack.isEmpty) {
-            widget.onEnd?.call();
-
-            if (widget.isLoop) {
-              _stack.addAll(widget.cards);
+              if (widget.isLoop) {
+                _stack.addAll(widget.cards);
+              }
             }
           }
         }
