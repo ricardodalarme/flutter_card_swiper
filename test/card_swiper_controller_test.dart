23c5e9f0d3aa9b18677d4ede6348377cac843411
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-03-25 16:14:38 -0300

    refactor(tests): update the controller test description

--- test/card_swiper_controller_test.dart
+++ test/card_swiper_controller_test.dart
@@ -4,31 +4,31 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('CardSwiperController', () {
-    test('swipe() changes state to swipe', () {
+    test('swipe() should change the state to swipe', () {
       final controller = CardSwiperController();
       controller.swipe();
       expect(controller.state, CardSwiperState.swipe);
     });
 
-    test('swipeLeft() changes state to swipeLeft', () {
+    test('swipeLeft() should change the state to swipeLeft', () {
       final controller = CardSwiperController();
       controller.swipeLeft();
       expect(controller.state, CardSwiperState.swipeLeft);
     });
 
-    test('swipeRight() changes state to swipeRight', () {
+    test('swipeRight() should change the state to swipeRight', () {
       final controller = CardSwiperController();
       controller.swipeRight();
       expect(controller.state, CardSwiperState.swipeRight);
     });
 
-    test('swipeTop() changes state to swipeTop', () {
+    test('swipeTop() should change the state to swipeTop', () {
       final controller = CardSwiperController();
       controller.swipeTop();
       expect(controller.state, CardSwiperState.swipeTop);
     });
 
-    test('swipeBottom() changes state to swipeBottom', () {
+    test('swipeBottom() should change the state to swipeBottom', () {
       final controller = CardSwiperController();
       controller.swipeBottom();
       expect(controller.state, CardSwiperState.swipeBottom);
